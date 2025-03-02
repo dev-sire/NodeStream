5c70d76a9951b17fa88e7709896bff4255851f2b
dev-sire <shahidamaan2019@gmail.com>
2025-03-02 23:48:25 +0500

    added carousel

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,23 @@
+import { HomeView } from "@/modules/home/ui/views/home-view";
 import { HydrateClient, trpc } from "@/trpc/server";
-import { PageClient } from "./client";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
+export const dynamic = "force-dynamic"
 
-export default async function Home() {
+interface PageProps{
+  searchParams: Promise<{
+    categoryId?: string,
+  }>;
+};
 
-  void await trpc.hello.prefetch({ text: "Aman" })
+const Page = async ({ searchParams }: PageProps) => {
+
+  const { categoryId } = await searchParams;
+  void trpc.categories.getMany.prefetch();
 
   return (
     <HydrateClient>
-      <Suspense fallback={<p>Loading...</p>}>
-      <ErrorBoundary fallback={<p>Error...</p>}>
-        <PageClient />
-      </ErrorBoundary>
-      </Suspense>
+      <HomeView categoryId={categoryId} />
     </HydrateClient> 
   );
 }
+
+export default Page;
---
+++ src/modules/home/ui/sections/categories-section.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { trpc } from "@/trpc/client";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import { FilterCarousel } from "@/components/filter-carousel";
+
+
+interface CategoriesSectionProps {
+  categoryId?: string;
+}
+
+export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
+  return(
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <CategoriesSectionSuspense categoryId={categoryId} />
+      </ErrorBoundary>
+    </Suspense>
+  )
+}
+
+const CategoriesSectionSuspense = ({ categoryId }: CategoriesSectionProps) => {
+
+  const [categories] = trpc.categories.getMany.useSuspenseQuery()
+
+  const data = categories.map(({ name, id }) => ({
+    value: id,
+    label: name,
+  }))
+
+  return <FilterCarousel isLoading value={categoryId} data={data} />
+}
