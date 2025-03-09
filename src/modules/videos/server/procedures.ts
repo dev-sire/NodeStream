2da28758dfd60f232baa5775cc60b31f971e9fa4
dev-sire <shahidamaan2019@gmail.com>
2025-03-09 23:01:44 +0500

    added Studio Uploader

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,10 +1,21 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
+import { mux } from "@/lib/mux";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const { id: userId } = ctx.user
+
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ["public"],
+        mp4_support: "standard",
+      },
+      cors_origin: "*", //TODO: In production, set to your url
+    })
+
     const [video] = await db.insert(videos).values({
       userId,
       title: "Untitled",
@@ -12,6 +23,7 @@ export const videosRouter = createTRPCRouter({
     .returning()
     return{
       video: video,
+      url: upload.url,
     }
   }),
 })
