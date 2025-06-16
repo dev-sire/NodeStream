3cf46943dcca6b92269fa3aa34e470ca6630ab7b
dev-sire <shahidamaan2019@gmail.com>
2025-06-16 21:26:00 +0500

    added proper skeletons

--- src/modules/suggestions/server/procedures.ts
+++ src/modules/suggestions/server/procedures.ts
@@ -2,7 +2,7 @@ import { db } from "@/db";
 import { users, videoReactions, videos, videoViews } from "@/db/schema";
 import { createTRPCRouter, baseProcedure } from "@/trpc/init";
 import { z } from "zod";
-import { eq, and, or, lt, desc, getTableColumns } from "drizzle-orm";
+import { eq, and, or, lt, desc, getTableColumns, not } from "drizzle-orm";
 import { TRPCError } from "@trpc/server";
 
 export const suggestionsRouter = createTRPCRouter({
@@ -48,6 +48,8 @@ export const suggestionsRouter = createTRPCRouter({
         .from(videos)
         .innerJoin(users, eq(videos.userId, users.id))
         .where(and(
+          not(eq(videos.id, existingVideo.id)),
+          eq(videos.visibility, "public"),
           existingVideo.categoryId
           ? eq(videos.categoryId, existingVideo.categoryId)
           : undefined,
--- src/modules/videos/ui/components/video-row-card.tsx
+++ src/modules/videos/ui/components/video-row-card.tsx
@@ -11,7 +11,7 @@ import {
 import { UserInfo } from "@/modules/users/ui/components/user-info";
 import { UserAvatar } from "@/components/user-avatar";
 import { VideoMenu } from "./video-menu";
-import { VideoThumbnail } from "./video-thumbnail";
+import { VideoThumbnail, VideoThumbnailSkeleton } from "./video-thumbnail";
 import { VideoGetManyOutput } from "../../types";
 
 const videoRowCardVariants = cva("group flex min-w-0", {
@@ -42,13 +42,42 @@ interface VideoRowCardProps extends VariantProps<typeof videoRowCardVariants>{
   onRemove?: () => void;
 }
 
-export const VideoRowCardSkeleton = () => {
-  return(
-    <div>
-      Skeleton
+export const VideoRowCardSkeleton = ({
+  size = "default",
+}: VariantProps<typeof videoRowCardVariants>) => {
+  return (
+    <div className={videoRowCardVariants({ size })}>
+      {/* Thumbnail skeleton */}
+      <div className={thumbnailVariants({ size })}>
+        <VideoThumbnailSkeleton />
+      </div>
+      {/* Info skeleton */}
+      <div className="flex-1 min-w-0">
+        <div className="flex justify-between gap-x-2">
+          <div className="flex-1 min-w-0">
+            <Skeleton
+              className={cn("h-5 w-[40%]", size === "compact" && "h-4 w-[40%]")}
+            />
+            {size === "default" && (
+              <>
+                <Skeleton className="h-4 w-[20%] mt-1" />
+                <div className="flex items-center gap-2 my-3">
+                  <Skeleton className="size-8 rounded-full" />
+                  <Skeleton className="h-4 w-24" />
+                </div>
+              </>
+            )}
+            {size === "compact" && (
+              <>
+                <Skeleton className="h-4 w-[50%] mt-1" />
+              </>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export const VideoRowCard = ({
   data,
