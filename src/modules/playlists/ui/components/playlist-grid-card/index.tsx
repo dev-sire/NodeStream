99228da48af9690704eb2d70ac7aa6f6c864e248
dev-sire <shahidamaan2019@gmail.com>
2025-07-10 23:47:16 +0500

    Section: Finished Playlists Section

--- src/modules/playlists/ui/components/playlist-grid-card/index.tsx
+++ src/modules/playlists/ui/components/playlist-grid-card/index.tsx
@@ -1,10 +1,20 @@
 import { PlaylistGetManyOutput } from "@/modules/playlists/types";
 import { THUMBNAIL_FALLBACK } from "@/modules/videos/types";
 import Link from "next/link";
-import { PlaylistThumbnail } from "./playlist-thumbnail";
+import { PlaylistThumbnail, PlaylistThumbnailSkeleton } from "./playlist-thumbnail";
+import { PlaylistInfo, PlaylistInfoSkeleton } from "./playlist-info";
 
 interface PlaylistGridCardProps {
   data: PlaylistGetManyOutput["items"][number];
+}
+
+export const PlaylistGridCardSkeleton = () => {
+  return(
+    <div className="flex flex-col gap-2 w-full">
+      <PlaylistThumbnailSkeleton />
+      <PlaylistInfoSkeleton />
+    </div>
+  )
 }
 
 export const PlaylistGridCard = ({ data }: PlaylistGridCardProps) => {
@@ -16,6 +26,7 @@ export const PlaylistGridCard = ({ data }: PlaylistGridCardProps) => {
           title={data.name}
           videoCount={data.videoCount}
         />
+        <PlaylistInfo data={data} />
       </div>
     </Link>
   )
